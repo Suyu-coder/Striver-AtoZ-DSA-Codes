71cc12418de83779e8d375ba5301ccc135f30e18
Suyash Jangam <60782268+Suyu-coder@users.noreply.github.com>
2025-06-04 16:41:27 +0530

    Create 02.lower bound in binary search.py

---
+++ 3.Binary Search/1.Easy/02.lower bound in binary search.py
@@ -0,0 +1,37 @@
+# lower bound in binary search
+
+#Brute Force Approach
+
+def lowerBound(arr,n,x):
+    for i in range(n):
+        if arr[i] >= x:
+            # lower bound found 
+            return i
+    return n 
+
+# Optimal Approach (Using Binary Search): 
+
+def lowerBound(arr,n,x):
+    low = 0
+    high = n-1
+    ans = n
+
+    while low <= high:
+        mid = (low+high) // 2
+
+        # may be an answer 
+        if arr[mid] >= x:
+            ans = mid 
+            # looking for smaller index 
+            high = mid-1 
+        else:
+            low = mid +1
+    return ans 
+
+
+if __name__ == "__main__":
+    arr = [3, 5, 8, 15, 19]
+    n = 5
+    x = 9
+    ind = lowerBound(arr, n, x)
+    print("The lower bound is the index:", ind)
